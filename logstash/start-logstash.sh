14fcbce5daa3acae6bbb4a5169224e732867ddac
Klaus Kämpf <kkaempf@suse.de>
2016-07-18 15:25:16 +0200

    fix running logstash from a different dir

--- logstash/start-logstash.sh
+++ logstash/start-logstash.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #  run logstash, config is in *.conf, watch config for changes
 export DIR=$(readlink -f ${0%/*})
+cd $(dirname $DIR)
 /opt/logstash/bin/logstash -f $DIR/\*.conf --auto-reload
