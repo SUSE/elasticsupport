2b203a282ac8df1039af08aa0c80e8b01dc58ba0
Klaus Kämpf <kkaempf@suse.de>
2016-07-29 13:11:59 +0200

    Use patterns_dir '.'

--- logstash/start-logstash.sh
+++ logstash/start-logstash.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #  run logstash, config is in *.conf, watch config for changes
 export DIR=$(readlink -f ${0%/*})
-cd $(dirname $DIR)
-/opt/logstash/bin/logstash -f $DIR/\*.conf --auto-reload
+echo "DIR $DIR"
+cd $DIR
+/opt/logstash/bin/logstash -f ./\*.conf --auto-reload
